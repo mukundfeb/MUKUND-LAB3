273183a469d71e2a7eea32519d626a4a4d4eeb11
Mukund S Ganig <mukunds.btech23@rvu.edu.in>
2025-11-12 12:37:33 +0530

    Create sqlQuery1.sql

---
+++ sqlQuery1.sql
@@ -0,0 +1,13 @@
+USE master;
+GO
+CREATE LOGIN etl WITH PASSWORD = 'demopass', DEFAULT_DATABASE = AdventureWorksDW2022;
+GO
+
+USE AdventureWorksDW2022;
+GO
+
+CREATE USER etl FOR LOGIN etl;
+GO
+
+GRANT CONNECT TO etl;
+GO
